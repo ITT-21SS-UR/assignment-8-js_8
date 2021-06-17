43ae8445b8eb593501e18023ea5c62353bf06303
Sarah Kurek <sarah.kurek@stud.uni-regensburg.de>
2021-06-17 20:56:20 +0200

    Create Nodes and setup Main Window.

---
+++ activity_recognizer.py
@@ -0,0 +1,147 @@
+import pyqtgraph as pg
+import numpy as np
+import sys
+import pyqtgraph.flowchart.library as fclib
+
+from enum import Enum
+from PyQt5 import uic
+from pyqtgraph.flowchart import Flowchart, Node
+from pyqtgraph.flowchart.library.common import CtrlNode
+from pyqtgraph.Qt import QtGui, QtCore
+from sklearn import svm
+from sklearn.exceptions import NotFittedError
+from scipy import signal
+
+from DIPPID import SensorUDP, SensorSerial, SensorWiimote
+from DIPPID_pyqtnode import BufferNode, DIPPIDNode
+
+
+
+class ModusNode(Enum): 
+    TRAINING = 1
+    PREDICTION = 2
+    INACTIVE = 3
+    
+class GestureNode(Node):
+    nodeName = "ActivityRecognition"
+
+    def __init__(self, name):
+        self.__state = ModusNode.INACTIVE
+        self.__is_recording = False
+
+        self.__sensor = SensorUDP()
+        self.__sensor.register_callback('button_1', self.handle_button_press)
+
+        terminals = {
+            'in': dict(io='in'), # TODO
+            'out': dict(io='out')
+        }
+        Node.__init__(self, name, terminals=terminals)
+
+    def set_recording(self, value):
+        self.__is_recording == value
+
+    def process(self, **kwds):
+        if self.__state == ModusNode.TRAINING:
+            self.train_model(kwds)
+
+        if self.__state == ModusNode.PREDICTION:
+            self.generate_prediction(kwds)
+
+    def train_model(kwds):
+        pass
+
+    def generate_prediction(kwds):
+        pass
+
+    def handle_button_press(self, data):
+        if int(data) == 0:
+            self.activity_node.set_recording(True)
+            print("button 1 pressed")
+        else:
+            self.activity_node.set_recording(False)
+        print("button 1 released")
+
+
+fclib.registerNodeType(GestureNode, [('Data', )])
+
+
+class FeatureExtractionNode(Node):
+    nodeName = "FeatureExtraction"
+
+    def __init__(self, name):
+        terminals = {
+            'in_x': dict(io='in'),
+            'in_y': dict(io='in'),
+            'in_z': dict(io='in'),
+            'out': dict(io='out')
+        }
+        Node.__init__(self, name, terminals=terminals)
+
+    def process(self, **kwds):
+        calc_x = np.fft.fft(kwds["in_x"])
+        calc_y = np.fft.fft(kwds["in_y"])
+        calc_z = np.fft.fft(kwds["in_z"])
+        print("x: " + calc_x + ", " + "y: " + calc_y + ", " + "z: " + calc_z)
+        return (calc_x, calc_y, calc_z)
+
+fclib.registerNodeType(FeatureExtractionNode, [('Data', )])
+
+
+def createAndConnectNodes(chart):
+    dippid_node = chart.createNode("DIPPID", pos=(0, 0))
+    buffer_node_accel_x = chart.createNode("Buffer", pos=(100, -200))
+    buffer_node_accel_y = chart.createNode("Buffer", pos=(130, -100))
+    buffer_node_accel_z = chart.createNode("Buffer", pos=(100, 200))
+    feature_extract_node = chart.createNode("FeatureExtraction", pos=(130, 100))
+    activity_node = chart.createNode("ActivityRecognition", pos=(150, 0))
+
+    chart.connectTerminals(
+        dippid_node['accelX'], buffer_node_accel_x['dataIn'])
+    chart.connectTerminals(
+        dippid_node['accelY'], buffer_node_accel_y['dataIn'])
+    chart.connectTerminals(
+        dippid_node['accelZ'], buffer_node_accel_z['dataIn'])
+    chart.connectTerminals(
+        buffer_node_accel_x['dataOut'], feature_extract_node['in_x'])
+    chart.connectTerminals(
+        buffer_node_accel_y['dataOut'], feature_extract_node['in_y'])
+    chart.connectTerminals(
+        buffer_node_accel_z['dataOut'], feature_extract_node['in_z'])
+
+    # TODO connect feature_extract_node['out']
+
+def setupWindow():
+    win = QtGui.QMainWindow()
+    win.setWindowTitle('Assignment 8')
+
+    central_widget = QtGui.QWidget()
+    win.setCentralWidget(central_widget)
+    layout = QtGui.QGridLayout()
+    central_widget.setLayout(layout)
+
+    chart = Flowchart(terminals={'out': dict(io='out')})
+    layout.addWidget(chart.widget(), 0, 0, 2, 1)
+    
+    createAndConnectNodes(chart)
+
+    win.show()
+
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        sys.exit(QtGui.QApplication.instance().exec_())
+
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        sys.stdout.write("Please specify port")
+
+    port_num = sys.argv[1]
+    print(port_num)
+
+    app = QtGui.QApplication([])
+    
+    setupWindow()
+
+    sys.exit(app.exec_())
